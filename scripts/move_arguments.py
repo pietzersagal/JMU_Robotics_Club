a88da68485bcfea510676bb56aee9b67df95371d
putzzach <73544687+putzzach@users.noreply.github.com>
2023-02-28 16:55:04 -0500

    Create move_arguments.py

---
+++ scripts/move_arguments.py
@@ -0,0 +1,36 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class Publisher(Node):
+    
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 1 #In seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+        
+    def timer_callback(self):
+        move = Twist()
+        move.linear.x = sys.argv[0]
+        move.linear.y = 0.0
+        move.linear.z = 0.0
+        move.angular.x = 0.0
+        move.angular.y = 0.0
+        move.angular.z = sys.argv[1]
+        
+        self.publisher.publish(move)
+        self.get_logger().info('Moving Bot: %d' % move)
+        self.i+=1
+        
+def main(args=None):
+    rclpy.init(args=args)
+    publisher = Publisher()
+    rclpy.spin(publisher)
+    
+    publisher.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
