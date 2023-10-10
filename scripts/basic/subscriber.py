5ede73b4b13159070e332e92f61ff85939f2ac4d
putzzach <73544687+putzzach@users.noreply.github.com>
2023-10-10 17:43:54 -0400

    Add files via upload

---
+++ scripts/basic/subscriber.py
@@ -0,0 +1,24 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+class Subscriber(Node):
+    
+    def __init__(self):
+        super().__init__('subscriber')
+        self.subscription = self.create_subscription(String, 'custom', self.subscriber_callback, 10)
+        
+    def subscriber_callback(self, msg):
+        #if(msg.linear.x != 0.0)
+        self.get_logger().info("%s" % str(msg.data))
+        
+def main(args=None):
+    rclpy.init(args=args)
+    subscriber = Subscriber()
+    rclpy.spin(subscriber)
+    publisher.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
